e8ecc2a7987c60f874d80e27b909f3ccb666602a
Geri <44909424+Geri3030@users.noreply.github.com>
2019-11-07 15:22:38 -0500

    Add files via upload

---
+++ clase2.X/build/default/debug/elegir.o.d
@@ -0,0 +1,3 @@
+build/default/debug/elegir.o:  \
+	C:/Users/GONZALO/MPLABXProjects/clase2.X/elegir.asm  \
+
---
+++ clase2.X/build/default/production/elegir.o.d
@@ -0,0 +1,3 @@
+build/default/production/elegir.o:  \
+	C:/Users/GONZALO/MPLABXProjects/clase2.X/elegir.asm  \
+
